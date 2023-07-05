cf230784c8783767103da879b7363044ec6b7d15
zhoujing01 <zhoujing01@jsfund.cn>
2023-07-05 17:01:10 +0800

    check royal flush and straight flush

--- src/utilities.ts
+++ src/utilities.ts
@@ -1,6 +1,12 @@
-import { Card } from './Card';
+import { Card, Rank, Shape } from './Card';
 
-export function getCountMapBySuit(cardList: Card[])  {
+const HAND_LENTH = 5;
+
+type CountMap = {
+  [s: string]: Card[]
+}
+
+export function getCountMapBySuit(cardList: Card[]): CountMap  {
   const countMap = {};
   cardList.forEach(card => {
     countMap[card.suit] = countMap[card.suit] || [];
@@ -9,11 +15,62 @@ export function getCountMapBySuit(cardList: Card[])  {
   return countMap;
 }
 
-export function getCountMapByRank(cardList: Card[]) {
+export function getCountMapByRank(cardList: Card[]): CountMap {
   const countMap = {}; // key: rank, value: list of cards
   cardList.forEach(card => {
     countMap[card.rank] = countMap[card.rank] || [];
     countMap[card.rank].push(card);
   });
   return countMap;
+}
+
+/**
+ * Get shape of cards
+ * @param cardList 
+ * @returns [Shape, Cards[]]
+ */
+export function getCardShape(cardList: Card[]): [Shape, Card[]] {
+  /**
+   * 
+   * @param sortedCardList in reverse order
+   * @returns straight cards
+   */
+  // function getStraight(sortedCardList: Card[]): Card[] | null {
+  //   if (sortedCardList.length < 5) {
+  //     return null;
+  //   }
+  //   for (let index = 0; index < sortedCardList.length; ++index) {
+  //     if (sortedCardList[index].rank - sortedCardList[index + HAND_LENTH - 1].rank === HAND_LENTH -1) {
+  //       return sortedCardList.slice(0, HAND_LENTH);
+  //     }
+  //   }
+  //   // special case A2345
+  //   if ()
+  // }
+
+
+  // check Royal Flush & Straight Flush
+  const suitCountMap = getCountMapBySuit(cardList);
+  const target = Object.entries(suitCountMap).find(([suit, cards]) => cards.length >= 5);
+  let possibleShape = Shape.HighCards;
+  if (target) {
+    // possible royal flush here
+    const [suit, cards] = target;
+    // sort these cards in reverse order
+    const cardsInReverseOrder = cards.sort((a, b) => a.rank > b.rank ? -1 : 0);
+    if (cardsInReverseOrder[HAND_LENTH - 1].rank === Rank.Ten) {
+      return [Shape.RoyalFlush, cardsInReverseOrder.slice(0, HAND_LENTH)];
+    }
+    // check straigh flush
+    for (let index = 0; index < cardsInReverseOrder.length - HAND_LENTH + 1; ++index) {
+      if (cardsInReverseOrder[index].rank - cardsInReverseOrder[index + HAND_LENTH - 1].rank === HAND_LENTH -1) {
+        return [Shape.StraightFlush, cardsInReverseOrder.slice(index, index + HAND_LENTH)];
+      }
+    }
+    // special case A2345
+    if (cardsInReverseOrder[0].rank === Rank.Ace && cardsInReverseOrder[cardsInReverseOrder.length - HAND_LENTH + 1].rank === 5) {
+      return [Shape.StraightFlush, [cardsInReverseOrder[0], ...cardsInReverseOrder.slice(cardsInReverseOrder.length - HAND_LENTH + 1, cardsInReverseOrder.length)]];
+    }
+  }
+  return [Shape.HighCards, []];
 }
--- test/utilities.test.ts
+++ test/utilities.test.ts
@@ -1,5 +1,5 @@
-import { Card, Rank, Suit } from '../src/Card';
-import { getCountMapByRank, getCountMapBySuit } from '../src/utilities';
+import { Card, Rank, Shape, Suit } from '../src/Card';
+import { getCardShape, getCountMapByRank, getCountMapBySuit } from '../src/utilities';
 
 test('get count map by suit', () => {
   const cardList = [
@@ -34,4 +34,70 @@ test('get count map by rank', () => {
   expect(countMap[Rank.Ace].length).toBe(4);
   expect(countMap[Rank.Two].length).toBe(1);
   expect(countMap[Rank.Three].length).toBe(1);
+})
+
+test('royal flush', () => {
+  const cardList = [
+    new Card(Suit.Club, Rank.Ace),
+    new Card(Suit.Club, Rank.Ten),
+    new Card(Suit.Club, Rank.Jack),
+    new Card(Suit.Club, Rank.Queen),
+    new Card(Suit.Club, Rank.King),
+    new Card(Suit.Diamond, Rank.Nine),
+  ];
+  const [shape] = getCardShape(cardList);
+  expect(shape).toBe(Shape.RoyalFlush);
+})
+
+test('not royal flush', () => {
+  const cardList = [
+    new Card(Suit.Club, Rank.Ace),
+    new Card(Suit.Club, Rank.Ten),
+    new Card(Suit.Club, Rank.Eight),
+    new Card(Suit.Club, Rank.Queen),
+    new Card(Suit.Club, Rank.King),
+    new Card(Suit.Diamond, Rank.Nine),
+  ];
+  const [shape] = getCardShape(cardList);
+  expect(shape).not.toBe(Shape.RoyalFlush);
+})
+
+test('straight flush', () => {
+  const cardList = [
+    new Card(Suit.Club, Rank.Ace),
+    new Card(Suit.Club, Rank.Ten),
+    new Card(Suit.Club, Rank.Eight),
+    new Card(Suit.Club, Rank.Nine),
+    new Card(Suit.Club, Rank.Six),
+    new Card(Suit.Club, Rank.Seven),
+    new Card(Suit.Diamond, Rank.Nine),
+  ];
+  const [shape, cards] = getCardShape(cardList);
+  expect(shape).toBe(Shape.StraightFlush);
+  expect(cards[0].suit).toBe(Suit.Club);
+  expect(cards[0].rank).toBe(Rank.Ten);
+  expect(cards[1].rank).toBe(Rank.Nine);
+  expect(cards[2].rank).toBe(Rank.Eight);
+  expect(cards[3].rank).toBe(Rank.Seven);
+  expect(cards[4].rank).toBe(Rank.Six);
+})
+
+test('straight flush A2345', () => {
+  const cardList = [
+    new Card(Suit.Club, Rank.Ace),
+    new Card(Suit.Club, Rank.Five),
+    new Card(Suit.Club, Rank.Three),
+    new Card(Suit.Club, Rank.Four),
+    new Card(Suit.Club, Rank.Two),
+    new Card(Suit.Club, Rank.Seven),
+    new Card(Suit.Diamond, Rank.Nine),
+  ];
+  const [shape, cards] = getCardShape(cardList);
+  expect(shape).toBe(Shape.StraightFlush);
+  expect(cards[0].suit).toBe(Suit.Club);
+  expect(cards[0].rank).toBe(Rank.Ace);
+  expect(cards[1].rank).toBe(Rank.Five);
+  expect(cards[2].rank).toBe(Rank.Four);
+  expect(cards[3].rank).toBe(Rank.Three);
+  expect(cards[4].rank).toBe(Rank.Two);
 })
