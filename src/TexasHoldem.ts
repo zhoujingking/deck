5b361f1b639737cc5400d92c00da0400b88d2ac8
zhoujing01 <zhoujing01@jsfund.cn>
2023-07-07 15:27:10 +0800

    added deck entity

--- src/TexasHoldem.ts
+++ src/TexasHoldem.ts
@@ -1,4 +1,5 @@
-import { Card } from './core/Card';
+import { Card, Rank, Suit } from './core/Card';
+import { shuffle } from 'lodash-es';
 
 /**
  * Represents hand type like Royal Flush, etc.
@@ -22,3 +23,40 @@ export type HoldemHand = {
 }
 
 export const CARD_LENGTH = 5;
+
+export class Deck {
+  private _cards: Array<Card>
+
+  constructor() {
+    this.reset();
+  }
+
+  private generateCards() {
+    const cards: Array<Card> = [];
+
+    [Suit.Spade, Suit.Heart, Suit.Diamond, Suit.Club].forEach(suit => {
+      [ Rank.Ace, Rank.King, Rank.Queen, Rank.Jack, 
+        Rank.Ten, Rank.Nine, Rank.Eight, Rank.Seven, 
+        Rank.Six, Rank.Five, Rank.Four, Rank.Three, Rank.Two].forEach(rank => {
+          cards.push(new Card(suit, rank))
+        })
+    })
+    return cards;
+  }
+
+  get cards() {
+    return this._cards;
+  }
+
+  reset() {
+    this._cards = this.generateCards();
+  }
+
+  shuffle(): Array<Card> {
+    return shuffle(this._cards);
+  }
+
+  pick(): Card {
+    return this._cards.shift();
+  }
+}
