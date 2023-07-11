8fa374b60d19e3159931d81987d052eb542c105f
zhoujing01 <zhoujing01@jsfund.cn>
2023-07-11 10:45:09 +0800

    website setup

---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+process.env.BABEL_ENV = 'development';
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    index: path.resolve(__dirname + '/website', 'index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    clean: true
+  },
+  devtool: 'source-map',
+  devServer: {
+    static: path.resolve('dist'),
+    hot: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.(js|jsx)$/i,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            ['babel-preset-react-app']
+          ]
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname + '/website', 'index.html')
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+      linkType: 'text/css'
+    }),
+  ],
+  optimization: {
+    runtimeChunk: 'single'
+  }
+}
---
+++ website/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function App(props) {
+  return (
+    <div>godking</div>
+  );
+}
+App.defaultProps={};
+App.propTypes={};
+export default App;
